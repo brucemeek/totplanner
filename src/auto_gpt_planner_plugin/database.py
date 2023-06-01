f1d52e43dd51c2801e8feed1d393d754f01b405e
brucemeek <113046530+brucemeek@users.noreply.github.com>
2023-06-01 16:08:07 +0000

    udate to files

--- src/auto_gpt_planner_plugin/database.py
+++ src/auto_gpt_planner_plugin/database.py
@@ -8,7 +8,6 @@ Base = declarative_base()
 # Define the Task model
 class Task(Base):
     __tablename__ = 'tasks'
-
     id = Column(Integer, primary_key=True)
     description = Column(String)
     priority = Column(Integer)
@@ -17,7 +16,6 @@ class Task(Base):
 # Define the Plan model
 class Plan(Base):
     __tablename__ = 'plans'
-
     id = Column(Integer, primary_key=True)
     goal = Column(String)
     completed = Column(Boolean)
@@ -27,11 +25,9 @@ class DatabaseManager:
     """
     The DatabaseManager class is responsible for managing the SQL databases. It interacts with the SQLAlchemy ORM to create, update, and retrieve data from the databases.
     """
-
     def __init__(self, db_name):
         """
         Initialize a new DatabaseManager instance.
-
         Args:
             db_name (str): The name of the SQLite database file.
         """
@@ -42,7 +38,6 @@ class DatabaseManager:
     def create_task(self, task):
         """
         Create a new task in the database.
-
         Args:
             task (Task): The task to be added to the database.
         """
@@ -53,10 +48,8 @@ class DatabaseManager:
     def get_task(self, task_id):
         """
         Retrieve a task from the database.
-
         Args:
             task_id (int): The ID of the task to be retrieved.
-
         Returns:
             Task: The retrieved task.
         """
@@ -67,7 +60,6 @@ class DatabaseManager:
     def update_task(self, task):
         """
         Update a task in the database.
-
         Args:
             task (Task): The task to be updated in the database.
         """
@@ -78,7 +70,6 @@ class DatabaseManager:
     def delete_task(self, task_id):
         """
         Delete a task from the database.
-
         Args:
             task_id (int): The ID of the task to be deleted.
         """
@@ -90,7 +81,6 @@ class DatabaseManager:
     def create_plan(self, plan):
         """
         Create a new plan in the database.
-
         Args:
             plan (Plan): The plan to be added to the database.
         """
@@ -101,10 +91,8 @@ class DatabaseManager:
     def get_plan(self, plan_id):
         """
         Retrieve a plan from the database.
-
         Args:
             plan_id (int): The ID of the plan to be retrieved.
-
         Returns:
             Plan: The retrieved plan.
         """
@@ -115,7 +103,6 @@ class DatabaseManager:
     def update_plan(self, plan):
         """
         Update a plan in the database.
-
         Args:
             plan (Plan): The plan to be updated in the database.
         """
@@ -131,10 +118,10 @@ class DatabaseManager:
             plan_id (int): The ID of the plan to be deleted.
         """
         session = self.Session()
-        plan = session.query(Plan).filter_by(id=plan_id)
-        first()
-        session.delete(plan)
-        session.commit()
+        plan = session.query(Plan).filter_by(id=plan_id).first()
+        if plan:
+            session.delete(plan)
+            session.commit()
 
     def mark_task_complete(self, task_id):
         """
@@ -145,8 +132,9 @@ class DatabaseManager:
         """
         session = self.Session()
         task = session.query(Task).filter_by(id=task_id).first()
-        task.completed = True
-        session.commit()
+        if task:
+            task.completed = True
+            session.commit()
 
     def mark_goal_complete(self, goal):
         """
@@ -157,8 +145,9 @@ class DatabaseManager:
         """
         session = self.Session()
         plan = session.query(Plan).filter_by(goal=goal).first()
-        plan.completed = True
-        session.commit()
+        if plan:
+            plan.completed = True
+            session.commit()
 
     def update_goals(self):
         """
@@ -166,3 +155,4 @@ class DatabaseManager:
         """
         # Implementation goes here
         pass
+
--- src/auto_gpt_planner_plugin/planner.py
+++ src/auto_gpt_planner_plugin/planner.py
@@ -2,11 +2,6 @@ from .database import DatabaseManager
 from .tasks import TaskManager
 
 class Planner:
-    """
-    The Planner class is responsible for managing the planning process.
-    It interacts with the TaskManager to create and manage tasks, and with the DatabaseManager to interact with the databases.
-    """
-
     def __init__(self):
         """
         Initializes the Planner class and creates new instances of the TaskManager and DatabaseManager.
@@ -18,75 +13,77 @@ class Planner:
         """
         Runs the initial planning cycle. This involves generating a new plan and task database, creating tasks based on the plan, and starting the execution of tasks.
         """
-        # Implementation goes here
-        pass
+        # Generate plan and task databases
+        self.generate_plan_database()
+        self.generate_task_database()
+
+        # Generate plan and tasks
+        plan = self.generate_plan()
+        tasks = self.generate_tasks(plan)
+
+        # Start execution of tasks
+        for task in tasks:
+            self.solve_task(task)
 
     def generate_plan_database(self):
         """
         Generates a new plan database for future use.
         """
-        # Implementation goes here
-        pass
+        self.database_manager.create_plan_database()
 
     def generate_task_database(self):
         """
         Generates a new task database that contains all tasks available to the plugin. This database will not be overwritten.
         """
-        # Implementation goes here
-        pass
+        self.database_manager.create_task_database()
 
     def generate_plan(self, goals):
         """
         Generates a new plan based on the given goals.
         """
-        # Implementation goes here
-        pass
+        return self.task_manager.generate_plan(goals)
 
     def generate_tasks(self, plan):
         """
         Generates unique tasks based on the new plan.
         """
-        # Implementation goes here
-        pass
+        return self.task_manager.generate_tasks(plan)
 
     def solve_task(self, task):
         """
         Solves the task with the highest priority using the solve method.
         """
-        # Implementation goes here
-        pass
+        self.task_manager.solve_task(task)
 
     def mark_task_complete(self, task):
         """
         Marks the given task as complete.
         """
-        # Implementation goes here
-        pass
+        self.task_manager.mark_task_complete(task)
 
     def update_task_database(self):
         """
         Updates the unique task database.
         """
-        # Implementation goes here
-        pass
+        self.database_manager.update_task_database()
 
     def complete_tasks_for_goal(self, goal):
         """
         Completes all tasks for a single goal.
         """
-        # Implementation goes here
-        pass
+        tasks = self.task_manager.get_tasks_for_goal(goal)
+        for task in tasks:
+            self.solve_task(task)
+            self.mark_task_complete(task)
 
     def mark_goal_complete(self, goal):
         """
         Marks the given goal as complete.
         """
-        # Implementation goes here
-        pass
+        self.database_manager.mark_goal_complete(goal)
 
     def update_goals(self):
         """
         Updates the goals to complete the overall goal.
         """
-        # Implementation goes here
-        pass
+        self.database_manager.update_goals()
